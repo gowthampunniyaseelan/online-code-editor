4081aac1177b037f426a690e704e88714cc51d3f
Gowtham Punniyaseelan <88957910+gowthampunniyaseelan@users.noreply.github.com>
2022-05-06 20:31:36 +0530

    react

--- editor/src/authentication/Signup.js
+++ editor/src/authentication/Signup.js
@@ -9,7 +9,7 @@ const Signup = () => {
 
   const handleSubmit = async (e)=>{
     e.preventDefault();
-    const response = await fetch('/api/signup',{
+    const response = await fetch('http://localhost:8000/api/signup',{
       method:'POST',
       headers:{
         "Content-Type":"application/json"
