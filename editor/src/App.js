e3bffb1d01baf1d2fea3c71a6a638a716dac553e
Gowtham Punniyaseelan <88957910+gowthampunniyaseelan@users.noreply.github.com>
2022-05-08 08:26:48 +0530

    auth

--- editor/src/App.js
+++ editor/src/App.js
@@ -1,23 +1,17 @@
-import React,{useState,useEffect} from 'react';
+import React from 'react';
 import { Page } from './views/Page';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // new imports
 import Signup from './authentication/Signup';
 import Login from './authentication/Login';
 import Home from './Home';
 function App() {
-  const [isAuthenticated,setIsAuthenticated] = useState(false);
-  useEffect(()=>{
-      setIsAuthenticated(localStorage.getItem('token'));
-  },[isAuthenticated])
   return(
     <Router>
     <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/signup" element={<Signup/>} />
+      <Route path="/" element={<Home/>}/>
+      <Route path="/signup" element={<Signup/>}/>
       <Route path="/login" element={<Login/>}/>
-      <Route path="/page" element={
-      isAuthenticated ? <Page/> : <Login/>
-      } />
+      <Route path="/page" element={<Page/>}/>
     </Routes>
     </Router>
   );
